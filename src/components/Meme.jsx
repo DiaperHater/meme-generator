a73a05beda49b4274ef4da63bb84003eaecb3158
Valery Pronin <pownmymail@gmail.com>
2023-01-16 00:11:01 +0300

    feat: show random image on button click

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,18 +1,31 @@
+import React from "react"
+import memesData from "../memesData"
+
 export default function Meme() {
+
+    const [imageUrl, setImageUrl] = React.useState('https://i.imgflip.com/30b1gx.jpg')
+
+    function getRandowImage(event) {
+        
+        const images = memesData.map(item => item.url)
+        const randomIndex = Math.floor(Math.random() * images.length)
+        const randomImageUrl = images[randomIndex]
+        setImageUrl(randomImageUrl)
+    }
 
     return (
         <section className="py-[36px]">
             <div className="wrapper">
-                <form className="meme">
+                <div className="mb-[36px] text-center">
                     <div className="flex space-x-4 mb-4">
                         <input type="text" name="top-text" id="top-text" className="border flex-grow rounded text-xs p-3" />
                         <input type="text" name="bottom-text" id="bottom-text" className="border flex-grow rounded text-xs p-3"/>
                     </div>
-                    <input 
-                        type="submit" name="submit" id="submit" value="Get a new meme image  🖼"
-                        className="bg-gradient w-full py-2 text-white font-bold rounded"
-                    />
-                </form>
+                    <button className="bg-gradient w-full py-2 text-white font-bold rounded cursor-pointer" onClick={getRandowImage}> 
+                        Get a new meme image  🖼
+                    </button>
+                </div>
+                <img src={imageUrl} alt="" className=""/>
             </div>
         </section>
     )
