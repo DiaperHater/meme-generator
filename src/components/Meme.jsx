01b6dfc7437f07280808c9f23d6d645ce653c067
Valery Pronin <pownmymail@gmail.com>
2023-01-17 15:31:01 +0300

    refactor: change state image -> meme object

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -3,14 +3,21 @@ import memesData from "../memesData"
 
 export default function Meme() {
 
-    const [imageUrl, setImageUrl] = React.useState('https://i.imgflip.com/30b1gx.jpg')
+    const [meme, setMeme] = React.useState(() => ({
+        topText: 'top text', 
+        bottomText: 'bottom text',
+        randomImage: 'https://i.imgflip.com/1ur9b0.jpg'
+    }))
 
-    function getRandowImage(event) {
+    function getMemeImage(event) {
         
-        const images = memesData.map(item => item.url)
-        const randomIndex = Math.floor(Math.random() * images.length)
-        const randomImageUrl = images[randomIndex]
-        setImageUrl(randomImageUrl)
+        const allImages = memesData.map(item => item.url)
+        const randomIndex = Math.floor(Math.random() * allImages.length)
+        const randomImageUrl = allImages[randomIndex]
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            randomImage: randomImageUrl
+        }))
     }
 
     return (
@@ -21,11 +28,11 @@ export default function Meme() {
                         <input type="text" name="top-text" id="top-text" className="border flex-grow rounded text-xs p-3" />
                         <input type="text" name="bottom-text" id="bottom-text" className="border flex-grow rounded text-xs p-3"/>
                     </div>
-                    <button className="bg-gradient w-full py-2 text-white font-bold rounded cursor-pointer" onClick={getRandowImage}> 
+                    <button className="bg-gradient w-full py-2 text-white font-bold rounded cursor-pointer" onClick={getMemeImage}> 
                         Get a new meme image  🖼
                     </button>
                 </div>
-                <img src={imageUrl} alt="" className=""/>
+                <img src={meme.randomImage} alt="" className="w-full"/>
             </div>
         </section>
     )
